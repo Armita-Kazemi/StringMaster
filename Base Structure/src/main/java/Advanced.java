e71a79ee5d601d4764aa02c282462b520d22a080
Armita Kazemi <armitakazemi55320@gmail.com>
2024-03-03 13:31:21 +0330

    word censor added

--- Base Structure/src/main/java/Advanced.java
+++ Base Structure/src/main/java/Advanced.java
@@ -10,7 +10,15 @@ public class Advanced {
 
      */
     public String wordCensor(String sentence, String word, String newWord){
-        return null;
+        String result="";
+        String[] words=sentence.split("\\s+");
+        for (int i=0;i<words.length;i++){
+            result+= (words[i].equals(word)) ? newWord : words[i];
+            if(i!= words.length-1){
+                result+=" ";//add space between words except for the last one
+            }
+        }
+        return result;
     }
 
     /**
@@ -32,4 +40,3 @@ public class Advanced {
         return null;
     }
 }
-
